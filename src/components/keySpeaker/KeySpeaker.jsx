914d623b3b629a6def864357b08b00886a2609a2
Gokul Nath <gokulnath0827@gmail.com>
2023-02-02 22:33:56 +0530

    fix

--- src/components/keySpeaker/KeySpeaker.jsx
+++ src/components/keySpeaker/KeySpeaker.jsx
@@ -9,24 +9,29 @@ const KeySpeaker = () => {
   <div className="card-flex">
     <div className="card">
         <img className="image" src={speaker1} alt="" />
-        <h1 className="name">Shreyas Raghunath</h1>
+        <h1 className="name" style={{textAlign:"center"}}>Shreyas Raghunath</h1>
         <p className="desig">Associate Software Engineer, Fox Corporation</p>
     </div>
     <div className="card">
         <img className="image" src={speaker2} alt="" />
-        <h1 className="name">Not said</h1>
+        <h1 className="name" style={{textAlign:"center"}}>Not said</h1>
         <p className="desig">Information Security Officer, PCCW Solutions</p>
     </div>
     <div className="card">
         <img className="image" src={speaker1} alt="" />
-        <h1 className="name">Dr. Asadi Srinivasulu</h1>
+        <h1 className="name" style={{textAlign:"center"}}>Dr. Asadi Srinivasulu</h1>
         <p className="desig">Professor of IT & Head Research, LabBlueCrest University</p>
     </div>
     <div className="card">
         <img className="image" src={speaker1} alt="" />
+        <h1 className="name" style={{textAlign:"center"}}>Dr. Asadi Srinivasulu</h1>
+        <p className="desig">Professor of IT & Head Research, LabBlueCrest University</p>
+    </div>
+    {/* <div className="card">
+        <img className="image" src={speaker1} alt="" />
         <h1 className="name">Not said</h1>
         <p className="desig">not said</p>
-    </div>
+    </div> */}
   </div>
 </div>
   )
