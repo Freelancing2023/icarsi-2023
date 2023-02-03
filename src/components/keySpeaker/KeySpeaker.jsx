39aa94f1df204516d860a8e6c94c62cd96d6c864
Gokul Nath <gokulnath0827@gmail.com>
2023-02-03 08:50:51 +0530

    image added

--- src/components/keySpeaker/KeySpeaker.jsx
+++ src/components/keySpeaker/KeySpeaker.jsx
@@ -1,41 +1,64 @@
-import React from 'react'
-import './keyspeaker.css'
-import speaker1 from "../../assets/speaker1.png"
-import speaker2 from "../../assets/speaker2.png"
+import React from "react";
+import "./keyspeaker.css";
+import speaker1 from "../../assets/speaker1.png";
+import speaker2 from "../../assets/speaker2.png";
+import speaker3 from "../../assets/speaker3.png";
+import speaker4 from "../../assets/speaker4.png";
 const KeySpeaker = () => {
   return (
-<div className="keyspeaker">
-  <h1 className='keynote-title'>Keynote Speakers</h1>
-  <div className="card-flex">
-    <div className="card">
-        <img className="image" src={speaker1} alt="" />
-        <h1 className="name" style={{textAlign:"center"}}>Shreyas Raghunath</h1>
-        <p className="desig">Associate Software Engineer, Fox Corporation</p>
-    </div>
-    <div className="card">
-        <img className="image" src={speaker2} alt="" />
-        <h1 className="name" style={{textAlign:"center"}}>Not said</h1>
-        <p className="desig">Information Security Officer, PCCW Solutions</p>
-    </div>
-    <div className="card">
-        <img className="image" src={speaker1} alt="" />
-        <h1 className="name" style={{textAlign:"center"}}>Dr. Asadi Srinivasulu</h1>
-        <p className="desig">Professor of IT & Head Research, LabBlueCrest University</p>
-    </div>
-    <div className="card">
-        <img className="image" src={speaker1} alt="" />
-        <h1 className="name" style={{textAlign:"center"}}>Dr. Asadi Srinivasulu</h1>
-        <p className="desig">Professor of IT & Head Research, LabBlueCrest University</p>
-    </div>
-    {/* <div className="card">
+    <div className="keyspeaker">
+      <h1 className="keynote-title">Keynote Speakers</h1>
+      <div className="card-flex">
+        <div className="card">
+          <img className="image" src={speaker1} alt="" />
+          <h1 className="name" style={{ textAlign: "center" }}>
+            Shreyas Raghunath
+          </h1>
+          <p className="desig">
+            Associate Software Engineer Fox Corporation Cincinnati, Ohio, United
+            States
+          </p>
+        </div>
+        <div className="card">
+          <img className="image" src={speaker2} alt="" />
+          <h1 className="name" style={{ textAlign: "center" }}>
+            Not said
+          </h1>
+          <p className="desig">
+            Information Security Officer, PCCW Solutions, Singapore.
+          </p>
+        </div>
+        <div className="card">
+          <img className="image" src={speaker3} alt="" />
+          <h1
+            className="name"
+            style={{ textAlign: "center", fontSize: "1rem" }}
+          >
+            Dr.Lochandaka Ranathunga
+          </h1>
+          <p className="desig">
+            Department of Information Technology, University of Moratuwa, Sri
+            Lanka
+          </p>
+        </div>
+        <div className="card">
+          <img className="image" src={speaker4} alt="" />
+          <h1 className="name" style={{ textAlign: "center" }}>
+            Dr. Asadi Srinivasulu
+          </h1>
+          <p className="desig">
+            Professor & HOD. Head Research of Data Science. Data Science
+            Research Laboratory BlueCrest
+          </p>
+        </div>
+        {/* <div className="card">
         <img className="image" src={speaker1} alt="" />
         <h1 className="name">Not said</h1>
         <p className="desig">not said</p>
     </div> */}
-  </div>
-</div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default KeySpeaker
-
+export default KeySpeaker;
