96baa062e9069f77e833821afe7b70bb5e3c765f
Gokul Nath <gokulnath0827@gmail.com>
2023-02-09 17:48:42 +0530

    fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { HashLink } from "react-router-hash-link";
 import { useState } from "react";
 import { CgMenuRight } from "react-icons/cg";
 import { CgClose } from "react-icons/cg";
-import file from '../../assets/Brocher.pdf'
+import file from '../../assets/Broucher.pdf'
 const Navbar = () => {
     const [colorChange, setColorchange] = useState(false)
     const [mobnavOpen, setMobilenavOpen] = useState(false)
