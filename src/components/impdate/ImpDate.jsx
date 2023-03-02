92be8b8511f674a39f1709506e91524af77a9393
Gokul Nath <gokulnath0827@gmail.com>
2023-03-02 14:25:27 +0530

    change

--- src/components/impdate/ImpDate.jsx
+++ src/components/impdate/ImpDate.jsx
@@ -11,29 +11,29 @@ const Impdate = () => {
         </div>
         <div className="imp-dates">
           <div className="row">
-            <h3 className="date">02<sup>nd</sup> Feburary 2023</h3>
-            <div className="bullet-design"><div className="bullet-design-hover" /></div>
             <h3 className="submission-of" style={{ width: '562px' }}>Paper Submission</h3>
+            <div className="bullet-design"><div className="bullet-design-hover" /></div>
+            <h3 className="date">02<sup>nd</sup> Feburary 2023</h3>
           </div>
           <div className="row">
-            <h3 className="date"> 01<sup>st</sup> March 2023 <br /> </h3>
-            <div className="bullet-design"><div className="bullet-design-hover" /></div>
             <h3 className="submission-of" style={{ width: '562px' }}>Last Date For Paper Submission</h3>
+            <div className="bullet-design"><div className="bullet-design-hover" /></div>
+            <h3 className="date"> 01<sup>st</sup> March 2023 <br /> </h3>
           </div>
           <div className="row">
-            <h3 className="date">10<sup>th</sup> March 2023 <br /> </h3>
-            <div className="bullet-design"><div className="bullet-design-hover" /></div>
             <h3 className="submission-of" style={{ width: '562px' }}>Acceptance Notification</h3>
+            <div className="bullet-design"><div className="bullet-design-hover" /></div>
+            <h3 className="date">10<sup>th</sup> March 2023 <br /> </h3>
           </div>
           <div className="row">
-            <h3 className="date">15<sup>th</sup> March 2023<br /></h3>
-            <div className="bullet-design"><div className="bullet-design-hover" /></div>
             <h3 className="submission-of" style={{ width: '562px' }}>Camera Ready Paper Submission & Early Bird Registration</h3>
+            <div className="bullet-design"><div className="bullet-design-hover" /></div>
+            <h3 className="date">15<sup>th</sup> March 2023<br /></h3>
           </div>
           <div className="row">
-            <h3 className="date">20<sup>th</sup> March 2023<br /></h3>
-            <div className="bullet-design"><div className="bullet-design-hover" /></div>
             <h3 className="submission-of" style={{ width: '562px' }}> Registrations Deadline  </h3>
+            <div className="bullet-design"><div className="bullet-design-hover" /></div>
+            <h3 className="date">20<sup>th</sup> March 2023<br /></h3>
           </div>
         </div>
       <div className="paperlink">
